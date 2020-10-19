d4e81c3a36dbc9ff47e786c7f231a2b08ca739e7
himanshu arora <himanshuar8898@gmail.com>
2020-10-19 17:11:56 +0530

    Initial upload of the project

--- product-detail.php
+++ product-detail.php
@@ -75,176 +75,172 @@
                 
                 <!-- Modal view content -->
                 <?php
-
-require "sqlconfig.php";
-?>
-<?php 
-$abc='';
-if (isset($_GET['id']) ) {
-$update=$_GET['id'];
-$abc=$update;
-$sql1="SELECT * from products WHERE  product_id='".$update."'";
-$result=$conn->query($sql1);
-if ($result->num_rows > 0) {
-    while ($row= $result->fetch_assoc()) {
-          if (gettype(unserialize($row['tags']))=='array' ) {
-         $test=unserialize($row['tags']);
-         foreach($test as $key=>$value){
-          
-if($abc>0){
-  $a='<div class="col-md-5 col-sm-5 col-xs-12">                              
-<div class="aa-product-view-slider">                                
-  <div id="demo-1" class="simpleLens-gallery-container">
-    <div class="simpleLens-container">
-      <div class="simpleLens-big-image-container"><a data-lens-image="img2/' . $row['image'] . '" class="simpleLens-lens-image"><img src="img/view-slider/medium/polo-shirt-1.png" class="simpleLens-big-image"></a></div>
-    </div>
-    <div class="simpleLens-thumbnails-container">
-        <a data-big-image="img/view-slider/medium/polo-shirt-1.png" data-lens-image="img/view-slider/large/polo-shirt-1.png" class="simpleLens-thumbnail-wrapper" href="#">
-          <img src="img/view-slider/thumbnail/polo-shirt-1.png">
-        </a>                                    
-        <a data-big-image="img/view-slider/medium/polo-shirt-3.png" data-lens-image="img/view-slider/large/polo-shirt-3.png" class="simpleLens-thumbnail-wrapper" href="#">
-          <img src="img/view-slider/thumbnail/polo-shirt-3.png">
-        </a>
-        <a data-big-image="img/view-slider/medium/polo-shirt-4.png" data-lens-image="img/view-slider/large/polo-shirt-4.png" class="simpleLens-thumbnail-wrapper" href="#">
-          <img src="img/view-slider/thumbnail/polo-shirt-4.png">
-        </a>
-    </div>
-  </div>
-</div>
-</div>';      
+                require "sqlconfig.php";
+                ?>
+                <?php 
+                $random='';
+                if (isset($_GET['id']) ) {
+                    $update=$_GET['id'];
+                    $random=$update;
+                    $sql1="SELECT * from products WHERE  product_id='".$update."'";
+                    $result=$conn->query($sql1);
+                    if ($result->num_rows > 0) {
+                        while ($row= $result->fetch_assoc()) {
+                            if (gettype(unserialize($row['tags']))=='array' ) {
+                                $test=unserialize($row['tags']);
+                                foreach ($test as $key=>$value) {
+                            
+                                    if ($random>0) {
+                                        $page='<div class="col-md-5 col-sm-5 col-xs-12">                              
+                                        <div class="aa-product-view-slider">                                
+                                          <div id="demo-1" class="simpleLens-gallery-container">
+                                            <div class="simpleLens-container">
+                                              <div class="simpleLens-big-image-container"><a data-lens-image="' . $row['image'] . '" class="simpleLens-lens-image"><img src="img/view-slider/medium/polo-shirt-1.png" class="simpleLens-big-image"></a></div>
+                                            </div>
+                                            <div class="simpleLens-thumbnails-container">
+                                                <a data-big-image="img/view-slider/medium/polo-shirt-1.png" data-lens-image="img/view-slider/large/polo-shirt-1.png" class="simpleLens-thumbnail-wrapper" href="#">
+                                                  <img src="img/view-slider/thumbnail/polo-shirt-1.png">
+                                                </a>                                    
+                                                <a data-big-image="img/view-slider/medium/polo-shirt-3.png" data-lens-image="img/view-slider/large/polo-shirt-3.png" class="simpleLens-thumbnail-wrapper" href="#">
+                                                  <img src="img/view-slider/thumbnail/polo-shirt-3.png">
+                                                </a>
+                                                <a data-big-image="img/view-slider/medium/polo-shirt-4.png" data-lens-image="img/view-slider/large/polo-shirt-4.png" class="simpleLens-thumbnail-wrapper" href="#">
+                                                  <img src="img/view-slider/thumbnail/polo-shirt-4.png">
+                                                </a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        </div>';      
    
-$a.=' <div class="col-md-7 col-sm-7 col-xs-12">
-<div class="aa-product-view-content">
-   <h3>' . $value . '</h3>
-  
-  <div class="aa-price-block">
-    <span class="aa-product-view-price">$' . $row['price'] . '</span>
-    <p class="aa-product-avilability">Avilability: <span>In stock</span></p>
-  </div>
-  <p>' . $row['discription'] . '</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis animi, veritatis quae repudiandae quod nulla porro quidem, itaque quis quaerat!</p>
-  <h4>Size</h4>
-  <div class="aa-prod-view-size">
-    <a href="#">S</a>
-    <a href="#">M</a>
-    <a href="#">L</a>
-    <a href="#">XL</a>
-  </div>
-  <h4> Selected--Color : ' . $row['color'] . '</h4>
-  <div class="aa-color-tag">
-    <a href="#" class="aa-color-green"></a>
-    <a href="#" class="aa-color-yellow"></a>
-    <a href="#" class="aa-color-pink"></a>                      
-    <a href="#" class="aa-color-black"></a>
-    <a href="#" class="aa-color-white"></a>                      
-  </div>
-  <div class="aa-prod-quantity">
-    <form action="">
-      <select id="" name="">
-        <option selected="1" value="0">1</option>
-        <option value="1">2</option>
-        <option value="2">3</option>
-        <option value="3">4</option>
-        <option value="4">5</option>
-        <option value="5">6</option>
-      </select>
-    </form>
-    <p class="aa-prod-category">
-      Category: <a href="#">' . $row['category_id'] . '</a>
-    </p>
-  </div>
-  <div class="aa-prod-view-bottom">
-    <a class="aa-add-to-cart-btn" href="#">Add To Cart</a>
-    <a class="aa-add-to-cart-btn" href="#">Wishlist</a>
-    <a class="aa-add-to-cart-btn" href="#">Compare</a>
-  </div>
-</div>
-</div>
-</div>
-</div>';
-echo $a;
+                                        $page=' <div class="col-md-7 col-sm-7 col-xs-12">
+                                        <div class="aa-product-view-content">
+                                          <h3>' . $value . '</h3>
+                                          
+                                          <div class="aa-price-block">
+                                            <span class="aa-product-view-price">$' . $row['price'] . '</span>
+                                            <p class="aa-product-avilability">Avilability: <span>In stock</span></p>
+                                          </div>
+                                          <p>' . $row['discription'] . '</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis animi, veritatis quae repudiandae quod nulla porro quidem, itaque quis quaerat!</p>
+                                          <h4>Size</h4>
+                                          <div class="aa-prod-view-size">
+                                            <a href="#">S</a>
+                                            <a href="#">M</a>
+                                            <a href="#">L</a>
+                                            <a href="#">XL</a>
+                                          </div>
+                                          <h4> Selected--Color : ' . $row['color'] . '</h4>
+                                          <div class="aa-color-tag">
+                                            <a href="#" class="aa-color-green"></a>
+                                            <a href="#" class="aa-color-yellow"></a>
+                                            <a href="#" class="aa-color-pink"></a>                      
+                                            <a href="#" class="aa-color-black"></a>
+                                            <a href="#" class="aa-color-white"></a>                      
+                                          </div>
+                                          <div class="aa-prod-quantity">
+                                            <form action="">
+                                              <select id="" name="">
+                                                <option selected="1" value="0">1</option>
+                                                <option value="1">2</option>
+                                                <option value="2">3</option>
+                                                <option value="3">4</option>
+                                                <option value="4">5</option>
+                                                <option value="5">6</option>
+                                              </select>
+                                            </form>
+                                            <p class="aa-prod-category">
+                                              Category: <a href="#">' . $row['category_id'] . '</a>
+                                            </p>
+                                          </div>
+                                          <div class="aa-prod-view-bottom">
+                                            <a class="aa-add-to-cart-btn" href="#">Add To Cart</a>
+                                            <a class="aa-add-to-cart-btn" href="#">Wishlist</a>
+                                            <a class="aa-add-to-cart-btn" href="#">Compare</a>
+                                          </div>
+                                        </div>
+                                        </div>
+                                        </div>
+                                        </div>';
+                                        echo $page;
 
-}
-         }
-         
+                                    }
+                                }
+                            }
 
-      }
+                            else{    
+                                $page='<div class="col-md-5 col-sm-5 col-xs-12">                              
+                                <div class="aa-product-view-slider">                                
+                                <div id="demo-1" class="simpleLens-gallery-container">
+                                <div class="simpleLens-container">
+                                <div class="simpleLens-big-image-container"><a data-lens-image="' . $row['image'] . '" class="simpleLens-lens-image"><img src="img/view-slider/medium/polo-shirt-1.png" class="simpleLens-big-image"></a></div>
+                                </div>
+                                <div class="simpleLens-thumbnails-container">
+                                      <a data-big-image="img/view-slider/medium/polo-shirt-1.png" data-lens-image="img/view-slider/large/polo-shirt-1.png" class="simpleLens-thumbnail-wrapper" href="#">
+                                      <img src="img/view-slider/thumbnail/polo-shirt-1.png">
+                                      </a>                                    
+                                      <a data-big-image="img/view-slider/medium/polo-shirt-3.png" data-lens-image="img/view-slider/large/polo-shirt-3.png" class="simpleLens-thumbnail-wrapper" href="#">
+                                      <img src="img/view-slider/thumbnail/polo-shirt-3.png">
+                                      </a>
+                                      <a data-big-image="img/view-slider/medium/polo-shirt-4.png" data-lens-image="img/view-slider/large/polo-shirt-4.png" class="simpleLens-thumbnail-wrapper" href="#">
+                                      <img src="img/view-slider/thumbnail/polo-shirt-4.png">
+                                                </a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        </div>';      
+                                          
+                                        $page.=' <div class="col-md-7 col-sm-7 col-xs-12">
+                                        <div class="aa-product-view-content">
+                                          <h3>' . $row['tags'] . '</h3>
+                                          <div class="aa-price-block">
+                                            <span class="aa-product-view-price">$' . $row['price'] . '</span>
+                                            <p class="aa-product-avilability">Avilability: <span>In stock</span></p>
+                                          </div>
+                                          <p>' . $row['discription'] . '</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis animi, veritatis quae repudiandae quod nulla porro quidem, itaque quis quaerat!</p>
+                                          <h4>Size</h4>
+                                          <div class="aa-prod-view-size">
+                                            <a href="#">S</a>
+                                            <a href="#">M</a>
+                                            <a href="#">L</a>
+                                            <a href="#">XL</a>
+                                          </div>
+                                          <h4> Selected--Color : ' . $row['color'] . '</h4>
+                                          <div class="aa-color-tag">
+                                            <a href="#" class="aa-color-green"></a>
+                                            <a href="#" class="aa-color-yellow"></a>
+                                            <a href="#" class="aa-color-pink"></a>                      
+                                            <a href="#" class="aa-color-black"></a>
+                                            <a href="#" class="aa-color-white"></a>                      
+                                          </div>
+                                          <div class="aa-prod-quantity">
+                                            <form action="">
+                                              <select id="" name="">
+                                                <option selected="1" value="0">1</option>
+                                                <option value="1">2</option>
+                                                <option value="2">3</option>
+                                                <option value="3">4</option>
+                                                <option value="4">5</option>
+                                                <option value="5">6</option>
+                                              </select>
+                                            </form>
+                                            <p class="aa-prod-category">
+                                              Category: <a href="#">' . $row['category_id'] . '</a>
+                                            </p>
+                                          </div>
+                                          <div class="aa-prod-view-bottom">
+                                            <a class="aa-add-to-cart-btn" href="#">Add To Cart</a>
+                                            <a class="aa-add-to-cart-btn" href="#">Wishlist</a>
+                                            <a class="aa-add-to-cart-btn" href="#">Compare</a>
+                                          </div>
+                                        </div>
+                                        </div>
+                                        </div>
+                                        </div>';
+                                        echo $page;
+                            }
 
- else{    
-
-$a='<div class="col-md-5 col-sm-5 col-xs-12">                              
-<div class="aa-product-view-slider">                                
-  <div id="demo-1" class="simpleLens-gallery-container">
-    <div class="simpleLens-container">
-      <div class="simpleLens-big-image-container"><a data-lens-image="img2/' . $row['image'] . '" class="simpleLens-lens-image"><img src="img/view-slider/medium/polo-shirt-1.png" class="simpleLens-big-image"></a></div>
-    </div>
-    <div class="simpleLens-thumbnails-container">
-        <a data-big-image="img/view-slider/medium/polo-shirt-1.png" data-lens-image="img/view-slider/large/polo-shirt-1.png" class="simpleLens-thumbnail-wrapper" href="#">
-          <img src="img/view-slider/thumbnail/polo-shirt-1.png">
-        </a>                                    
-        <a data-big-image="img/view-slider/medium/polo-shirt-3.png" data-lens-image="img/view-slider/large/polo-shirt-3.png" class="simpleLens-thumbnail-wrapper" href="#">
-          <img src="img/view-slider/thumbnail/polo-shirt-3.png">
-        </a>
-        <a data-big-image="img/view-slider/medium/polo-shirt-4.png" data-lens-image="img/view-slider/large/polo-shirt-4.png" class="simpleLens-thumbnail-wrapper" href="#">
-          <img src="img/view-slider/thumbnail/polo-shirt-4.png">
-        </a>
-    </div>
-  </div>
-</div>
-</div>';      
-   
-$a.=' <div class="col-md-7 col-sm-7 col-xs-12">
-<div class="aa-product-view-content">
-  <h3>' . $row['tags'] . '</h3>
-  <div class="aa-price-block">
-    <span class="aa-product-view-price">$' . $row['price'] . '</span>
-    <p class="aa-product-avilability">Avilability: <span>In stock</span></p>
-  </div>
-  <p>' . $row['discription'] . '</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis animi, veritatis quae repudiandae quod nulla porro quidem, itaque quis quaerat!</p>
-  <h4>Size</h4>
-  <div class="aa-prod-view-size">
-    <a href="#">S</a>
-    <a href="#">M</a>
-    <a href="#">L</a>
-    <a href="#">XL</a>
-  </div>
-  <h4> Selected--Color : ' . $row['color'] . '</h4>
-  <div class="aa-color-tag">
-    <a href="#" class="aa-color-green"></a>
-    <a href="#" class="aa-color-yellow"></a>
-    <a href="#" class="aa-color-pink"></a>                      
-    <a href="#" class="aa-color-black"></a>
-    <a href="#" class="aa-color-white"></a>                      
-  </div>
-  <div class="aa-prod-quantity">
-    <form action="">
-      <select id="" name="">
-        <option selected="1" value="0">1</option>
-        <option value="1">2</option>
-        <option value="2">3</option>
-        <option value="3">4</option>
-        <option value="4">5</option>
-        <option value="5">6</option>
-      </select>
-    </form>
-    <p class="aa-prod-category">
-      Category: <a href="#">' . $row['category_id'] . '</a>
-    </p>
-  </div>
-  <div class="aa-prod-view-bottom">
-    <a class="aa-add-to-cart-btn" href="#">Add To Cart</a>
-    <a class="aa-add-to-cart-btn" href="#">Wishlist</a>
-    <a class="aa-add-to-cart-btn" href="#">Compare</a>
-  </div>
-</div>
-</div>
-</div>
-</div>';
-echo $a;
-}
-
-    }
-}
-}
+                        }
+                    }
+                }
 ?>   
             <div class="aa-product-details-bottom">
               <ul class="nav nav-tabs" id="myTab2">
